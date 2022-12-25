4085805fb3c071df1a1f79a54d38cd3b183b7f54
m_malakouti <malakouti.aero@gmail.com>
2022-12-25 12:01:35 +0330

    The class modified.

--- src/main/java/com/example/accountservice/model/AccountEntity.java
+++ src/main/java/com/example/accountservice/model/AccountEntity.java
@@ -1,13 +1,33 @@
 package com.example.accountservice.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AccountEntity {
-    private Long Number;
+    private Long number;
     private String name;
+
+    public AccountEntity(Long number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public AccountEntity() {
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
